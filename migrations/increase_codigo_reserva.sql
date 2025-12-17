858164d010236b4c4030076a048f88244db4fe09
Andy Gamberg <andygamberg@MacBook-Air-M3-Andy.local>
2025-12-17 10:00:29 -0300

    chore: eliminar archivos obsoletos (MVP4 research, migrations, refactor plan)

--- migrations/increase_codigo_reserva.sql
+++
@@ -1,6 +0,0 @@
--- Migración: Aumentar límite de codigo_reserva de VARCHAR(50) a VARCHAR(255)
--- Razón: Algunos códigos de expediciones/charters son más largos (ej: Antarctica_Photo_Expedition_with_AntonioS.Chamorro_2026)
--- Fecha: 15 Dic 2025
-
-ALTER TABLE viaje
-ALTER COLUMN codigo_reserva TYPE VARCHAR(255);
